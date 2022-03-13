5900a196414d9f77570bfe4eca996af35a685c57
KseverNikita <45142706+KseverNikita@users.noreply.github.com>
2022-03-13 16:29:56 +0300

    update names

--- loader.py
+++ loader.py
@@ -3,15 +3,14 @@ import torchvision
 from torchvision import transforms
 from torchvision.datasets import CelebA
 
-def get_dataset(args, split='train')
+def get_data_loader(args, split='train')
     """
     Loads CelebA dataset from torchvision and applies proproccesing steps to it.
     Input: - args
            - split: 'train'/'test' - running mode
-    Output: - TrainDataLoader
-            - TestDataLoader
+    Output: - TrainDataLoader / TestDataLoader
     """
-    # Data preprocessing.
+    # Creating transforms.
     if split == 'train':
         transform = transforms.Compose([transforms.Resize(128),
                                           transforms.CenterCrop(128),
@@ -23,12 +22,11 @@ def get_dataset(args, split='train')
                                         transforms.Normalize((0.5, 0.5, 0.5),
                                         (0.5, 0.5, 0.5))])
 
-    # Create the dataset.
+    # Create the dataset and loader.
     
     dataset = CelebA(root='./celeba', split=split, download=True, transform=transform) 
     
     loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_sz,
                                          shuffle=(split == 'train'), num_workers=args.num_workers)
-
     return loader
 
