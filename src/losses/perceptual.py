c806200b94c93cfb027f04f09be7d2865e724ade
BobrG <bobrovskihg@gmail.com>
2022-03-14 16:12:49 +0300

    corrected vgg loss class

--- src/losses/perceptual.py
+++ src/losses/perceptual.py
@@ -1,9 +1,9 @@
 import torch
 import torchvision
-# code from here: https://gist.github.com/alper111/8233cdb0414b4cb5853f2f730ab95a49
+# code is inspired by implementation: https://gist.github.com/alper111/8233cdb0414b4cb5853f2f730ab95a49
 
 class VGGPerceptualLoss(torch.nn.Module):
-    def __init__(self, recon_function, resize=True, vgg='16'): # TODO: add vgg='19'
+    def __init__(self, feature_loss_func, resize=True, vgg='16'): # TODO: add vgg='19'
         super(VGGPerceptualLoss, self).__init__()
         blocks = []
         blocks.append(torchvision.models.vgg16(pretrained=True).features[:4].eval())
@@ -18,6 +18,7 @@ class VGGPerceptualLoss(torch.nn.Module):
         self.resize = resize
         self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
         self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
+        self.feature_loss_func = feature_loss_func
 
     def forward(self, input, target, feature_layers=[0, 1, 2, 3]):
         if input.shape[1] != 3:
@@ -28,11 +29,12 @@ class VGGPerceptualLoss(torch.nn.Module):
         if self.resize:
             input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
             target = self.transform(target, mode='bilinear', size=(224, 224), align_corners=False)
+        
         loss = 0.0
         x = input
         y = target
         for i, block in enumerate(self.blocks):
             x = block(x)
             y = block(y)
-            loss += recon_function(x, y) / (x.shape[2] * x.shape[3])
+            loss += feature_loss_func(x, y) / (x.shape[2] * x.shape[3])
         return loss
